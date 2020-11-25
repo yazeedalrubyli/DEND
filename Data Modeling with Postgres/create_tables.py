22e36fe57232983e80fa2ad4b4dbe34f28d483a0
unknown <yazeed.alrubyli@gmail.com>
2020-11-25 04:12:01 +0300

    Added project

--- Data Modeling with Postgres/create_tables.py
+++ Data Modeling with Postgres/create_tables.py
@@ -3,6 +3,10 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def create_database():
+    '''
+    Drop database (sparkifydb) if exists, create a new one.
+    :returns: The cursor and connection for (sparkifydb).
+    '''
     # connect to default database
     conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
     conn.set_session(autocommit=True)
@@ -23,21 +27,35 @@ def create_database():
 
 
 def drop_tables(cur, conn):
+    '''
+    Drop tables based on sql_queries.py.
+    :param cur: A cursor to perform database operations.
+    :param conn: Connection to an existing database.
+    '''
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    '''
+    Create tables based on sql_queries.py.
+    :param cur: A cursor to perform database operations.
+    :param conn: Connection to an existing database.
+    '''
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    '''
+    Drop the database, create a new one then create the tables based on sql_queries.py.
+    '''
     cur, conn = create_database()
     
-    drop_tables(cur, conn)
+    # No need for droping all tables one by one, we already droping the whole database(sparkifydb) in previous function call line 21
+    #drop_tables(cur, conn)
     create_tables(cur, conn)
 
     conn.close()
--- Data Modeling with Postgres/sql_queries.py
+++ Data Modeling with Postgres/sql_queries.py
@@ -1,52 +1,98 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP table songplays"
+user_table_drop = "DROP table users"
+song_table_drop = "DROP table songs"
+artist_table_drop = "DROP table artists"
+time_table_drop = "DROP table time"
 
 # CREATE TABLES
-
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays
+    (songplay_id SERIAL PRIMARY KEY NOT NULL, 
+    user_id INT REFERENCES users(user_id), 
+    song_id VARCHAR (255) REFERENCES songs(song_id), 
+    artist_id VARCHAR (255) REFERENCES artists(artist_id), 
+    start_time TIMESTAMP REFERENCES time(start_time),
+    level VARCHAR (4) NOT NULL CHECK (level in ('free', 'paid')),
+    location TEXT NOT NULL, 
+    session_id INT NOT NULL,
+    user_agent TEXT NOT NULL)
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users
+    (user_id INT PRIMARY KEY,
+    first_name VARCHAR (255) NOT NULL,
+    last_name VARCHAR (255) NOT NULL, 
+    gender VARCHAR (1) NOT NULL CHECK (gender in ('M', 'F')), 
+    level VARCHAR (4) NOT NULL CHECK (level in ('free', 'paid')))
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs
+    (song_id VARCHAR (255) PRIMARY KEY,
+    title TEXT NOT NULL, 
+    artist_id VARCHAR (20) REFERENCES artists(artist_id), 
+    year INT NOT NULL, 
+    duration FLOAT NOT NULL)
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists
+    (artist_id VARCHAR (255) PRIMARY KEY,
+    name VARCHAR (255) NOT NULL, 
+    location TEXT NOT NULL, 
+    latitude FLOAT, 
+    longitude FLOAT)
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time
+    (start_time TIMESTAMP PRIMARY KEY,
+    hour INT, 
+    day INT, 
+    week INT, 
+    month INT, 
+    year INT, 
+    weekday VARCHAR (10) CHECK (weekday in ('Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')))
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays (user_id, song_id, artist_id, start_time, level, location, session_id, user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (songplay_id) DO NOTHING;
 """)
 
 user_table_insert = ("""
+    INSERT INTO users VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING;
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs VALUES (%s, %s, %s, %s, %s);
 """)
 
 artist_table_insert = ("""
+    INSERT INTO artists VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING;
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO time VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time) DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
+    SELECT song_id, songs.artist_id FROM songs
+    JOIN artists ON songs.artist_id = artists.artist_id
+    WHERE title      = %s
+    AND artists.name = %s
+    AND duration     = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [user_table_drop, song_table_drop, artist_table_drop, time_table_drop, songplay_table_drop]
